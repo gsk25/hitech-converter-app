76b1b8fb2554793fc08acebfc51876cdbb52ea5b
SaiKrishna <sai.gollapalli@gmail.com>
2020-05-24 20:20:47 +0200

    Created a sample spring boot application to create rest services.

---
+++ converter-app/src/main/java/org/hitech/converterapp/ConverterAppApplication.java
@@ -0,0 +1,13 @@
+package org.hitech.converterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterAppApplication.class, args);
+	}
+
+}
---
+++ converter-app/src/test/java/org/hitech/converterapp/ConverterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hitech.converterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
